3bf80b511a69acc0a674aba3aeeeca0f405f7359
Chan531 <chan657312@gmail.com>
2023-10-16 21:33:06 +0900

    feat : 기본 과제

---
+++ seminar/src/main/java/com/server/dosopt/seminar/dto/HealthCheckResponse.java
@@ -0,0 +1,15 @@
+package com.server.dosopt.seminar.dto;
+
+import lombok.Getter;
+
+@Getter
+public class HealthCheckResponse {
+
+    private static final String STATUS = "OK";
+
+    private String status;
+
+    public HealthCheckResponse() {
+        this.status = STATUS;
+    }
+}
